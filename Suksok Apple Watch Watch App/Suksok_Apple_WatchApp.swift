8a159f4faa910c89f5057e5789f164f200e8175e
Yeski <yeskialex@gmail.com>
2025-11-03 19:05:35 +0900

    Initial Commit

---
+++ Suksok Apple Watch Watch App/Suksok_Apple_WatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Suksok_Apple_WatchApp.swift
+//  Suksok Apple Watch Watch App
+//
+//  Created by Yehezkiel Alexander on 11/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Suksok_Apple_Watch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
